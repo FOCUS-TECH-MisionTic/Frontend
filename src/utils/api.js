24483613ba6c4dabbe4670a0d363e1d17f4d4f9d
Ezequiel <lopezriosezequiel@gmail.com>
2021-10-26 10:36:17 -0500

    URLS

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-const baseURL = "http://localhost:5000";
-//const baseURL = 'https://.herokuapp.com';
+// const baseURL = "http://localhost:5000";
+const baseURL = 'https://polar-anchorage-11845.herokuapp.com/';
 
 const getToken = () => {
   return `Bearer ${localStorage.getItem('token')}`;
