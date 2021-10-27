905a40f7b4d392c067bc075250c663d836f4f0cb
Ezequiel <lopezriosezequiel@gmail.com>
2021-10-26 22:33:28 -0500

    update

--- src/layouts/PrivateLayout.jsx
+++ src/layouts/PrivateLayout.jsx
@@ -75,26 +75,3 @@ const PrivateLayout = ({ children }) => {
  };
 
 export default PrivateLayout;
-         
-
-// import React from 'react';
-// import Sidebar from '../components/Sidebar';
-// import PrivateRoute from 'components/PrivateRoute';
-
-// const PrivateLayout = ({ children }) => {
-//   return (
-//     <PrivateRoute>
-//       <div className='flex w-screen h-screen'>
-//         <div className='flex flex-nowrap h-full w-full'>
-//           <Sidebar />
-//           <main className='bg-gradient-to-r from-gray-400 via-gray-300 to-gray-400 flex w-full  overflow-auto items-center justify-center pr-0 '>
-//             {children}
-//           </main>
-//         </div>
-//       </div>
-//     </PrivateRoute> 
-//   );
-// };
-
-// export default PrivateLayout;
-
